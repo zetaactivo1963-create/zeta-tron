51a2388733fe94b3c4b1679140e2b288b1b34e66
zetaactivo1963-create <zetaactivo1963@gmail.com>
2026-01-06 23:22:12 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function HomePage() {
           href={`/events/${featuredEvent.slug}`}
           style={{
             textDecoration: "none",
-            color: "#ffffff",
+            color: "#0ff",
             display: "block",
           }}
         >
