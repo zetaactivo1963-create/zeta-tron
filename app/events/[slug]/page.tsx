9cbdc03b05c90d23c8895f2eeef96dbbe583c58e
DJ EDY <zetaactivo1963@gmail.com>
2026-02-11 13:22:30 -0400

    Update page.tsx

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,9 +1,13 @@
-"use client";
-
 import { redirect } from 'next/navigation';
 
-export default function EventoPage({ params }: { params: { slug: string } }) {
-  if (params.slug === 'zeta-grid-2') {
+export default async function EventoPage({ 
+  params 
+}: { 
+  params: Promise<{ slug: string }> 
+}) {
+  const { slug } = await params;
+  
+  if (slug === 'zeta-grid-2') {
     redirect('/events/zeta-grid-2');
   }
   
